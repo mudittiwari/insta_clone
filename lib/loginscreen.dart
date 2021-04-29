21060ef9a5c30336f9fb14fc3e13aa44563e7165
mudit <mudit19.072@gmail.com>
2021-04-29 11:35:59 +0530

    first commit

---
+++ lib/loginscreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class loginscreen extends StatefulWidget {
+  @override
+  _loginscreenState createState() => _loginscreenState();
+}
+
+class _loginscreenState extends State<loginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
